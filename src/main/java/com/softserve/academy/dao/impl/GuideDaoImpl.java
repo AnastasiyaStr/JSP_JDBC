96d186af19f857ccea115bf59e0504e301fd015c
Anast <nastya_strong@ukr.net>
2019-04-12 22:48:57 +0300

    add adding guide to database

--- src/main/java/com/softserve/academy/dao/impl/GuideDaoImpl.java
+++ src/main/java/com/softserve/academy/dao/impl/GuideDaoImpl.java
@@ -1,14 +1,33 @@
 package com.softserve.academy.dao.impl;
 
 import com.softserve.academy.dao.GuideDao;
+import com.softserve.academy.db.Database;
 import com.softserve.academy.entity.GuideEntity;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class GuideDaoImpl implements GuideDao {
     @Override
     public boolean saveGuide(GuideEntity guide) {
-        return false;
+
+        try (PreparedStatement insertToGuide = Database.getInstance()
+                .getConnection()
+                .prepareStatement("INSERT INTO guide(id_position,firstname,lastname)" +
+                        "VALUES(?,?,?)")
+        ) {
+            insertToGuide.setInt(1, 2);
+            insertToGuide.setString(2, guide.getFirstname());
+            insertToGuide.setString(3, guide.getLastname());
+            insertToGuide.execute();
+            System.out.println("Successfully added new guide: " + guide.getFirstname() + " " + guide.getLastname());
+        } catch (SQLException e) {
+            System.out.println("Oh...");
+            return false;
+        }
+        return true;
     }
 
     @Override
---
+++ src/main/webapp/add.jsp
@@ -0,0 +1,10 @@
+<form method="post">
+    <label>Name:
+        <input type="text" name="firstname"><br />
+    </label>
+
+    <label>Password:
+        <input type="text" name="lastname"><br />
+    </label>
+    <button type="submit">Submit</button>
+</form>
